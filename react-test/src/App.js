8aee7b3c2d552b5cc3b1fbedefab7ec9365650a1
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-06-10 13:03:16 +0530

    Card components

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import { Navbar } from '../node_modules/reactstrap';
 import './App.css';
 import NavbarBrand from "reactstrap/es/NavbarBrand";
 import Menu from './components/MenuComponent';
+import {DISHES} from "./shared/dishes";
+
 // Bootsrap component added into the code using reatstrap
 
-function App() {
-  return (
-    <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">
-              Siddharth Datthathreya Blog
-            </NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
-    </div>
-  );
+class App extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dishes:DISHES
+        };
+    }
+    render() {
+        return (
+            <div >
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">
+                            Siddharth Datthathreya Blog
+                        </NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu dishes={this.state.dishes }/>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
