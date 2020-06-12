f43d55bbee81d25cc4e5f4396df32526b12ec868
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-06-12 13:27:24 +0530

    Header and Footer Component

--- react-test/src/components/MenuComponent.js
+++ react-test/src/components/MenuComponent.js
@@ -13,7 +13,6 @@ function RednerMenuItem({dish, onClick}) {
 }
 
 const Menu = (props) => {
-
     const menu = props.dishes.map((dish) =>{
         return(
             <div key={dish.id} className="col-12 col-md-5 m-1">
